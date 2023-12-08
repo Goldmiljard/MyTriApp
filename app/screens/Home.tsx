15a9a9741c610b9274dd46af90be9842fa456a9c
Rick Kasper <rickkas90@gmail.com>
2023-12-08 11:01:48 +0100

    conditional rendering of strava authentication

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -3,15 +3,34 @@ import { COLORS } from "../../constants";
 import { MyText } from "../../components/Generic/";
 import StravaAuthentication from "../../components/Home/StravaAuthentication";
 import Logo from "../../components/Generic/Logo";
+import { useEffect, useState } from "react";
+import useFetch from "../../hook/useFetch";
 
 export default function Home() {
+  const [isStravaAuthenticated, setStravaAuthenticated] = useState(false);
+
+  useEffect(() => {
+    useFetch("/StravaAuthentication", "GET")
+      .then((response) => {
+        console.log(response.data);
+        setStravaAuthenticated(response.data);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Logo></Logo>
       <MyText bold={true} style={styles.homeText}>
         Welcome to My Tri!
       </MyText>
-      <StravaAuthentication></StravaAuthentication>
+      {!isStravaAuthenticated ? (
+        <StravaAuthentication></StravaAuthentication>
+      ) : (
+        <></>
+      )}
     </SafeAreaView>
   );
 }
